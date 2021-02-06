f30bc5752e1feae8007b56ba86cf260d949deb90
Heather Creighton <heather.creighton@gmail.com>
2021-02-06 12:02:41 -0500

    adds a welcome message

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Welcome from './Welcome'
+
+
+function App() {
+  return (
+    <div className="container">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome() {
+    return <h2>Welcome Michigan Wolverine Fans!</h2>
+}
+
+export default Welcome;
