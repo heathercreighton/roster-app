1722c88387878e15a7f06e9cad56dd9a56db4749
Heather Creighton <heather.creighton@gmail.com>
2021-02-06 12:30:39 -0500

    adds players object

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import logo from '../logo.svg';
 import '../App.css';
 import Welcome from './Welcome'
+import { playerData } from '../data/playerData'
+import Player from './Player'
 
 
+
+console.log(playerData);
 function App() {
   return (
     <div className="container">
       <Welcome />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,8 @@
+function Player(props) {
+    return (
+        <p>Players</p>
+    )
+}
+
+
+export default Player;
